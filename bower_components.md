52054b1b8dd30da926e40c20aba90fbdfba62146
Pontus Rulander <pontus.rulander@cegedim.com>
2014-01-03 17:36:37 +0100

    Drawing widgets from JSOn file

---
+++ bower_components.md
@@ -0,0 +1,8 @@
+#Bower Components Installed
+
+- angular
+- angular-resource
+- angular-route
+- angular-sanatize
+- bootstrap
+- jquery
--- js/cheat_sheet.app.js
+++ js/cheat_sheet.app.js
@@ -1,4 +1,10 @@
-var cheatSheet = angular.module('cheatSheet', ['ngRoute']);
+var cheatSheet = angular.module('cheatSheet', ['ngRoute','ngSanitize']);
+
+
+cheatSheet.factory('TemplateService', function($resource) {
+  return $resource('cats.json');
+});
+
 
 // configure our routes
 cheatSheet.config(function ($routeProvider, $locationProvider ) {
@@ -11,9 +17,16 @@ cheatSheet.config(function ($routeProvider, $locationProvider ) {
         }).when('/end2end', {
             templateUrl: 'views/E2ETesting.html',
             controller: 'e2etestingController'
+        }).when('/widgets', {
+            templateUrl: 'views/widgetlist.html',
+            controller: 'widgetlistController'
         })
 
+
 });
+
+
+
 
 
 cheatSheet.controller('mainController', function ($scope) {
@@ -26,3 +39,24 @@ cheatSheet.controller('e2etestingController', function ($scope) {
 $scope.jumboTitle = 'End-2-End Testing';
     $scope.jumboMessage = 'DRC:s Cheat Sheet for setting up and writing End-2-End Tests with Protractor.';
 });
+
+
+cheatSheet.controller('widgetlistController', function ($scope, $http) {
+    $scope.jumboTitle = 'List of widgets';
+    $scope.jumboMessage = 'Test to se if we can list different widgets with different templates';
+    
+    $http.get('/data/widgets.json').success(function(data) {
+    $scope.widgets = data;
+        //console.log(data);
+  });
+});
+
+var templates ={
+    "links":"templates/link_list_widget.html",
+    "sections":"templates/section_list_widget.html"
+};
+
+cheatSheet.controller('WidgetController', function ($scope) {
+    $scope.template = templates[$scope.widget.type];
+});
+
