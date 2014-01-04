22b06f7b9101749aa0816f5defa58cd44fe6c1c4
Pontus Rulander <poru1027@gmail.com>
2014-01-04 04:01:26 +0100

    Added widgetTagFilter

--- js/cheat_sheet.app.js
+++ js/cheat_sheet.app.js
@@ -9,6 +9,44 @@ cheatSheet.factory('WidgetService', function ($resource) {
   return $resource('/data/widgets.json');
 });
 
+cheatSheet.filter('widgetTag', function() {
+    
+    function isString( variable ) {
+        return typeof variable === 'string'; 
+    } 
+    
+    function isArray( variable ) {
+        return Object.prototype.toString.call( variable ) === '[object Array]'; 
+    }
+    
+    function taglistHasTag(taglist, tag ) {
+        return taglist.indexOf(tag) !== -1
+    }
+    
+    function widgethasTags( widget, tags  ) {
+        
+        for( var i = 0; i < tags.length; i++) {
+            if( taglistHasTag( widget.tags, tags[i] )) {return true; }
+        }
+        
+        // No specified tag was found on widget
+        return false;    
+    }
+    
+    return function(input, tags) {
+        // Guard aginst no tags
+        if( !tags  ) { return input; }
+        // Guard aginst single string tag
+        if(isString( tags )) { tags = [tags]; }
+        // Guard agints tags is not an array
+        if( !isArray(tags) ) { return input;  }
+        
+      return input.filter(function(widget, index){
+          return widgethasTags(widget, tags);
+      });
+    }
+  });
+
 cheatSheet.config(function ($routeProvider, $locationProvider) {
     //$locationProvider.html5Mode(true);
     $routeProvider
@@ -26,7 +64,6 @@ cheatSheet.config(function ($routeProvider, $locationProvider) {
 });
 
 
-
 cheatSheet.controller('mainController', function ($scope) {
     $scope.jumboTitle = 'Writing Good Code!';
     $scope.jumboMessage = 'DRC:s checklists and quick reference sheet for producing redable, maintainable, extendable and testable code!';
@@ -39,12 +76,12 @@ $scope.jumboTitle = 'End-2-End Testing';
 });
 
 
-cheatSheet.controller('widgetlistController', function ($scope, WidgetService) {
+cheatSheet.controller('widgetlistController', function ($scope, WidgetService, widgetTagFilter ) {
     $scope.jumboTitle = 'List of widgets';
     $scope.jumboMessage = 'Test to se if we can list different widgets with different templates';
     
     WidgetService.query(function(widgets){
-        $scope.widgets = widgets;
+        $scope.widgets = widgetTagFilter(widgets, ['e2e'] );
     });
 });
 
